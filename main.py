0e6166288626b880ad89a173d607f5b126aa8ec8
noura <noura@gmail.com>
2022-05-18 10:45:51 +0300

    Modules and packages lab

---
+++ main.py
@@ -0,0 +1,9 @@
+## Create a new module "main.py" , and do the following:
+#- import dateOP
+#- call the function that print the current date
+
+### hint : You should import the date class from the datetime module.
+
+import dateOP
+
+dateOP.current_data()

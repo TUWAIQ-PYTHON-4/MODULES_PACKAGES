ea02572b6265393cc3e6135656d513df51a6c84f
Hawra Alomani <ha0073@mix.wvu.edu>
2022-05-18 10:49:45 +0300

    finished module lab

---
+++ dateOP.py
@@ -0,0 +1,6 @@
+import datetime
+# A function that when called prints the current date.
+
+def current_date():
+    print(datetime.date.today())
+
---
+++ main.py
@@ -0,0 +1,4 @@
+import dateOP
+
+#call the function that print the current date
+dateOP.current_date()
